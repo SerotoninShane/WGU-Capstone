8dbd4551e2f4ff925e6aa05ea951e0dee92956af
Shane Bogue <90866675+SerotoninShane@users.noreply.github.com>
2025-04-30 13:29:23 -0700

    Update ml_models.py

--- python/ml_models.py
+++ python/ml_models.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 
 # Load the dataset
-df = pd.read_csv("./data/books.csv")
+df = pd.read_csv("python/data/books.csv")
 
 # Fill missing values
 df.fillna("", inplace=True)
