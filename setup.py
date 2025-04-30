36fc5e9e3dfa6ffd951d67512e2fdccc03d564b8
Shane Bogue <90866675+SerotoninShane@users.noreply.github.com>
2025-04-30 13:25:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+try:
+    long_description = open('README.md').read()
+except FileNotFoundError:
+    long_description = 'A book recommendation system based on KNN and cosine similarity.'
+
 setup(
     name='recommended',
     version='0.1',
@@ -10,10 +15,11 @@ setup(
         'pandas',
         'numpy',
     ],
-    description='A book recommendation system based on various algorithms like KNN and Cosine similarity.',
-    long_description=open('README.txt').read(),
-    long_description_content_type='text/plain',
+    python_requires='>=3.7',
+    description='A book recommendation system based on KNN and Cosine similarity.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Shane Bogue',
     author_email='sbogue8@wgu.edu',
-    url='https://github.com/SerotoninShane/WGU-Capstone',  # Replace with your GitHub link
+    url='https://github.com/SerotoninShane/WGU-Capstone',
 )
